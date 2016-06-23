fdc48c271127ac794d9c8b97c928007d2b3b87c4
Igor Knyazev <nulleof@gmail.com>
2016-06-24 03:00:26 +0600

    Simple untested ios plugin method

---
+++ Appodeal/Appodeal.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+#import "RCTLog.h"
+
+@interface Appodeal : NSObject <RCTBridgeModule>
+
+@end
---
+++ Appodeal/Appodeal.m
@@ -0,0 +1,17 @@
+#import "Appodeal.h"
+
+@interface Appodeal ()
+-(NSString*) debug;
+@end
+
+@implementation Appodeal
+
+RCT_EXPORT_METHOD()
+//RCT_EXPORT_MODULE(Appodeal) //they are equal
+
+RCT_EXPORT_METHOD(debug:(NSString *) message) {
+    RCTLogInfo(@"debug method was called for message %@", message);
+    NSLog(@"NSLog for message %@", message);
+}
+
+@end
